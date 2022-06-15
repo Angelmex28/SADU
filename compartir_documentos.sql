9ba861a65285236336457ea1166e6f8bb2ee1dc1
Juan Acosta <angelmex121@gmail.com>
2022-06-15 19:11:27 -0400

    Cambios en la db

--- compartir_documentos.sql
+++ compartir_documentos.sql
@@ -1,56 +1,55 @@
--- phpMyAdmin SQL Dump
--- version 5.1.3
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 28-05-2022 a las 16:02:15
--- Versión del servidor: 10.4.24-MariaDB
--- Versión de PHP: 8.1.5
+/*{------------------------------------------------------------------------
+|  MySQL Database Backup DumpDB
+|
+|  Host: localhost    Database: compartir_documentos    CreateAt: 2022-06-14 19:34:33
+|
+|  Server: version 10.4.24-MariaDB
+|
+|  Author of DumpDB: < gil_yeung@outlook.com > Gilberto Villarreal Rodriguez 
+*-------------------------------------------------------------------------}*/
+ 
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+-- ================================== BEGIN DATABASE compartir_documentos =======================================
+-- DROP DATABASE IF EXISTS `compartir_documentos`;
+-- CREATE DATABASE `compartir_documentos`;
+-- USE `compartir_documentos`;
 
---
--- Base de datos: `compartir_documentos`
---
+-- DROP TABLE tablename1, tablename2, tablename3, tablename4;
 
--- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `documents`
---
-
+-- ----------------------------
+-- Table structure for documents
+-- ----------------------------
+ 
+DROP TABLE IF EXISTS `documents`;
 CREATE TABLE `documents` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(200) NOT NULL,
   `description` text NOT NULL,
   `file_json` text NOT NULL,
   `user_id` int(30) NOT NULL,
-  `date_created` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+ 
+-- ----------------------------
+-- Records for table documents
+-- ----------------------------
+ 
+INSERT INTO `documents` (`id`,`title`,`description`,`file_json`,`user_id`,`date_created`) VALUES
+('3','Este es un super documento de prueba','Se puede escribir y adjunta como si fuera un correo sin problema.','[\"1632264840_1.pdf\",\"1632264840_3.pdf\",\"1632264840_2.pdf\"]','1','2021-09-21 17:55:03'),
+('4','Prueba 1','n1','[\"1655249340_Lineamientos Uptntms.pdf\"]','3','2022-06-14 19:29:40');
 
---
--- Volcado de datos para la tabla `documents`
---
-
-INSERT INTO `documents` (`id`, `title`, `description`, `file_json`, `user_id`, `date_created`) VALUES
-(3, 'Este es un super documento de prueba', 'Se puede escribir y adjunta como si fuera un correo sin problema.', '[\"1632264840_1.pdf\",\"1632264840_3.pdf\",\"1632264840_2.pdf\"]', 1, '2021-09-21 17:55:03');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `users`
---
-
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+ 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `id` int(30) NOT NULL,
+  `id` int(30) NOT NULL AUTO_INCREMENT,
   `firstname` varchar(200) NOT NULL,
   `lastname` varchar(200) NOT NULL,
   `middlename` varchar(200) NOT NULL,
@@ -60,51 +59,20 @@ CREATE TABLE `users` (
   `password` text NOT NULL,
   `type` tinyint(1) NOT NULL DEFAULT 2 COMMENT '1=Admin,2= users',
   `avatar` text NOT NULL,
-  `date_created` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4;
+ 
+-- ----------------------------
+-- Records for table users
+-- ----------------------------
+ 
+INSERT INTO `users` (`id`,`firstname`,`lastname`,`middlename`,`contact`,`address`,`email`,`password`,`type`,`avatar`,`date_created`) VALUES
+('3','Administrador','','','04268274205','8331','juanacosta@gmail.com','81dc9bdb52d04dc20036dbd8313ed055','1','1655246340_logo sadu.jpg','0000-00-00 00:00:00'),
+('11','qwwe','eeeqwe','wewqewqewq','12131312','weqweqe','123@123.com','202cb962ac59075b964b07152d234b70','2','1','0000-00-00 00:00:00'),
+('34','juan','acosta','jose','04268274205','calle 13 luis herrera','juan@juan.com','81dc9bdb52d04dc20036dbd8313ed055','2','0','2016-05-22 00:00:00');
+/*{}*/
+-- ================================== END DATABASE compartir_documentos ========================================
 
---
--- Volcado de datos para la tabla `users`
---
-
-INSERT INTO `users` (`id`, `firstname`, `lastname`, `middlename`, `contact`, `address`, `email`, `password`, `type`, `avatar`, `date_created`) VALUES
-(3, 'Craig', 'Phillips', 'acosta', '04268274205', '8331', 'juanacosta@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', 1, '1652233080_online_graduation_icon_150061.png', '0000-00-00 00:00:00'),
-(11, 'qwwe', 'eeeqwe', 'wewqewqewq', '12131312', 'weqweqe', '123@123.com', '202cb962ac59075b964b07152d234b70', 2, '1', '0000-00-00 00:00:00'),
-(34, 'juan jose', 'rico', 'acosta', '04268274205', 'calle 13 luis herrera', 'juan@juan.com', '81dc9bdb52d04dc20036dbd8313ed055', 2, '0', '2016-05-22 00:00:00');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `documents`
---
-ALTER TABLE `documents`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `documents`
---
-ALTER TABLE `documents`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT de la tabla `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ 
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
